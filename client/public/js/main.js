210efb027c7978e2a969a4567050c55fc7a29c5a
Ivan <iv.xromov@mail.ru>
2018-11-24 20:14:44 +0300

    changed socket host

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,11 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var io = require('socket.io-client');
 
-const locally = true;
+const locally = false;
 if (locally) {
   var socket = io('http://localhost');
 } else {
-  var socket = io('http://chs-server.herokuapp.com');
+  var socket = io('http://185.228.234.10');
 }
 
 module.exports = socket;
