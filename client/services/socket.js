f899f7c64aa1f01e197544946741eda6dd301274
ivan <iv.xromov@mail.ru>
2019-02-11 21:45:31 +0300

    port magic

--- client/services/socket.js
+++ client/services/socket.js
@@ -3,7 +3,7 @@ var io = require('socket.io-client');
 const locally = false;
 
 if (locally){
-  var socket = io('http://localhost');
+  var socket = io('http://localhost:3561');
 } else {
   // var socket = io('http://chs-server.herokuapp.com');
 //   var socket = io('http://185.228.234.10');
