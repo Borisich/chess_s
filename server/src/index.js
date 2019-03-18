c58208613c1983813a4b82bf61055af51bcc243b
kurnev.e <kurnev.e@gmail.com>
2019-03-18 14:37:30 +0300

    moved to public

--- server/src/index.js
+++ server/src/index.js
@@ -10,37 +10,6 @@ io.attach(server, {
 });
 
 app.set("port", process.env.WS_PORT || 3561);
-app.use(express.static(path.join(__dirname, "../client/public")));
-app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://185.228.234.10");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.get("/", function(request, response) {
-  //response.send('Hello man');
-  console.log("request!");
-  response.sendFile(path.join(__dirname, "../client/public", "index.html"));
-  // response.sendFile('../client/public/index.html');
-});
 
 var db = require("./database.js");
 
